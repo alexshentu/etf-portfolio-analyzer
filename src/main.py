36638c9cd7c1ccaae3c4462ca05aad75855b8290
Alex Shen Tu <shentualex@gmail.com>
2026-07-16 17:34:32 +0800

    Add VOO data download using yfinance

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+import yfinance as yf
+
 print ("Hello ETF Portfolio Analyzer")
+
+ticker = "VOO"
+data = yf.download(ticker, period="1y")
+VOO_close_data = data["Close"]
+print(VOO_close_data.iloc[0])
+print(type(data))
+print(type(VOO_close_data))
