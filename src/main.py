ddf9c7df1cdd3a18dc939927bbe9cab39dddc967
Alex Shen Tu <shentualex@gmail.com>
2026-07-17 13:05:49 +0800

    Add volatility and total return

--- src/main.py
+++ src/main.py
@@ -7,5 +7,23 @@ daily_return = close_data.pct_change()
 
 best_day = daily_return.idxmax()
 best_return = daily_return.max()
+
+worst_day = daily_return.idxmin()
+worst_return = daily_return.min()
+
+daily_volatility = daily_return.std()
+annual_volatility = daily_volatility * (252 ** 0.5)
+
+growth_factor = daily_volatility + 1
+total_return = growth_factor.cumprod().iloc[-1] - 1
+
 print (f"Best Day: {best_day}")
-print (f"Return: {best_return:.2%}")
+print (f"Best Return: {best_return:.2%}")
+
+print (f"Worst Day: {worst_day}")
+print (f"Worst Return: {worst_return:.2%}")
+
+print (f"Daily Volatility: {daily_volatility:.2%}")
+print (f"Annual Volatility: {annual_volatility:.2%}")
+
+print (f"Total Return: {total_return:.2%}")
