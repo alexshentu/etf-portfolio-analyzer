578a6d8dbdc23c4af492929a503a94fbd5a71946
Alex Shen Tu <shentualex@gmail.com>
2026-07-18 16:02:33 +0800

    Add moving average plot and crossover signals

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 #download info 
 ticker = "VOO"
@@ -27,7 +28,11 @@ daily_drawdown = (close_data - historical_high) / historical_high
 maximum_drawdown = daily_drawdown.min()
 MDD_day = daily_drawdown.idxmin()
 
-#output
+#moving average
+moving_average_20 = close_data.rolling(20).mean()
+moving_average_50 = close_data.rolling(50).mean()
+
+#data output
 print (f"Best Day: {best_day}")
 print (f"Best Return: {best_return:.2%}")
 print()
@@ -41,3 +46,53 @@ print (f"Total Return: {total_return:.2%}")
 print()
 print (f"Maximum Drawdown Day: {MDD_day}")
 print(f"Maximum Drawdown: {maximum_drawdown:.2%}")
+print()
+print(f"20-Day Moving Average: {moving_average_20.iloc[-1]:.2f}")
+print(f"50-Day Moving Average: {moving_average_50.iloc[-1]:.2f}")
+
+#plot
+close_data.plot(label = "Close Price", 
+                color = "black", 
+                linewidth = 2)
+
+moving_average_20.plot(label = "20-Day MA", 
+                       color = "blue", 
+                       linewidth = 1.5, 
+                       linestyle = "--")
+
+moving_average_50.plot(label = "50-Day MA", 
+                       color = "red", 
+                       linewidth = 1.5, 
+                       linestyle = "--")
+
+#golden cross
+golden_cross = (
+    (moving_average_20 > moving_average_50)
+    & (moving_average_20.shift(1) <= moving_average_50.shift(1)))
+
+plt.scatter(close_data[golden_cross].index,
+            close_data[golden_cross],
+            color="green",
+            s=120,
+            label="Golden Cross", 
+            marker = "^")
+
+#death cross
+death_cross = (
+    (moving_average_20 < moving_average_50)
+    & (moving_average_20.shift(1) >= moving_average_50.shift(1)))
+
+plt.scatter(close_data[death_cross].index,
+            close_data[death_cross],
+            color="red",
+            s=120,
+            label="Death Cross", 
+            marker = "v")
+
+plt.title("VOO Price Analysis")
+plt.xlabel("Date")
+plt.ylabel("Price (USD)")
+plt.grid()
+plt.legend()
+plt.show()
+
